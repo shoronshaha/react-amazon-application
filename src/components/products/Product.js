6f1f512cb2d90a6741d6fdc8edfdb2358916bff9
shoronshaha <smaranshaha@gmail.com>
2022-03-25 03:46:00 +0600

    event handelar added

--- src/components/products/Product.js
+++ src/components/products/Product.js
@@ -3,6 +3,7 @@ import './Product.css'
 
 const Product = (props) => {
     const { name, seller, price, ratings, img } = props.product;
+
     return (
         <div className='product'>
             <img src={img} alt="" />
@@ -12,7 +13,7 @@ const Product = (props) => {
                 <p><small>Seller: {seller}</small></p>
                 <p><small>Ratings: {ratings} stars</small></p>
             </div>
-            <button className='btn-cart'>
+            <button onClick={() => props.handleAddToCart(props.product)} className='btn-cart'>
                 <p>Add to Cart</p>
             </button>
         </div>
--- src/components/shop/Shop.js
+++ src/components/shop/Shop.js
@@ -11,14 +11,19 @@ const Shop = () => {
             .then(data => setProducts(data))
     }, [])
 
+    const handleAddToCart = (product) => {
+        console.log(product);
+    }
+
     return (
         <div className='shop-container'>
             <div className="products-container">
                 {
-                    products.map(product => <Product key={product.id}
-                        product={product}>
-
-                    </Product>)
+                    products.map(product => <Product
+                        key={product.id}
+                        product={product}
+                        handleAddToCart={handleAddToCart}
+                    ></Product>)
                 }
             </div>
             <div className="cart-container">
