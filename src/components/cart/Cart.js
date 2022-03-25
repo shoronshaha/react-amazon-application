5021c9222017b3d8b53d2d1d35a30bdcba965535
shoronshaha <smaranshaha@gmail.com>
2022-03-25 23:52:27 +0600

    updated

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './Cart.css'
-const Cart = ({ cart }) => {
+const Cart = (props) => {
+    const { cart } = props;
     return (
-        <div>
+        <div className='cart'>
             <h3>Order summary</h3>
-            <p>Selected Items : {cart.length} </p>
+            <p>Selected Items: {cart.length} </p>
+            <p>Total price:</p>
+            <p>Total Shipping: </p>
+            <h4>Grand Total: $</h4>
         </div>
     );
 };
