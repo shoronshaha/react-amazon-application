2b6e438531a4d635f2e1eedd342b7c3e22bd6730
shoronshaha <smaranshaha@gmail.com>
2022-03-27 18:03:46 +0600

    toral price updated

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -2,11 +2,16 @@ import React from 'react';
 import './Cart.css'
 const Cart = (props) => {
     const { cart } = props;
+    console.log(cart)
+    let total = 0;
+    for (const product of cart) {
+        total = total + product.price;
+    }
     return (
         <div className='cart'>
             <h3>Order summary</h3>
             <p>Selected Items: {cart.length} </p>
-            <p>Total price:</p>
+            <p>Total price: ${total} </p>
             <p>Total Shipping: </p>
             <h4>Grand Total: $</h4>
         </div>
