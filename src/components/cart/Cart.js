2a5977e4c803eb602658b36fcc8f12bb29862f90
shoronshaha <smaranshaha@gmail.com>
2022-03-27 18:06:31 +0600

    shipping added

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -4,15 +4,17 @@ const Cart = (props) => {
     const { cart } = props;
     console.log(cart)
     let total = 0;
+    let shipping = 0;
     for (const product of cart) {
         total = total + product.price;
+        shipping = shipping + product.shipping;
     }
     return (
         <div className='cart'>
             <h3>Order summary</h3>
             <p>Selected Items: {cart.length} </p>
             <p>Total price: ${total} </p>
-            <p>Total Shipping: </p>
+            <p>Total Shipping: {shipping}</p>
             <h4>Grand Total: $</h4>
         </div>
     );
