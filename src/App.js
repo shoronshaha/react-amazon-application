2dcfbbf695c92afc5cc1b3080aac0faa6e8ce592
shoronshaha <smaranshaha@gmail.com>
2022-03-30 23:16:49 +0600

    Route install and added all path with nav

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './components/header/Header';
 import Shop from './components/shop/Shop';
+import Orders from './components/orders/Orders';
+import Inventory from './components/inventory/Inventory';
+import About from './components/about/About';
 
 
 function App() {
   return (
     <div>
       <Header></Header>
-      <Shop></Shop>
+      <Routes>
+        <Route path="/" element={<Shop />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/orders" element={<Orders />} />
+        <Route path="/inventory" element={<Inventory />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
     </div>
   );
 }
