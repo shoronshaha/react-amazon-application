49728a9271c64baaaf52791947cdb4ccd94a7792
shoronshaha <smaranshaha@gmail.com>
2022-03-31 14:26:04 +0600

    orders length updated

--- src/components/orders/Orders.js
+++ src/components/orders/Orders.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import useCart from '../../hooks/useCart';
+import useProducts from '../../hooks/useProducts';
 
 const Orders = () => {
+    const [products, setProducts] = useProducts();
+    const [cart, setCart] = useCart(products);
     return (
         <div>
-            <h1>this is Orders</h1>
+            <h1>this is Orders: {products.length}</h1>
+            <p>Cart has:{cart.length}</p>
         </div>
     );
 };
--- src/components/shop/Shop.js
+++ src/components/shop/Shop.js
@@ -1,18 +1,13 @@
 import React, { useEffect, useState } from 'react';
+import useProducts from '../../hooks/useProducts';
 import { addToDb, getStoredCart } from '../../utilities/fakedb'
 import Cart from '../cart/Cart';
 import Product from '../products/Product';
 import './Shop.css'
 
 const Shop = () => {
-    const [products, setProducts] = useState([])
+    const [products, setProducts] = useProducts()
     const [cart, setCart] = useState([])
-
-    useEffect(() => {
-        fetch('products.json')
-            .then(res => res.json())
-            .then(data => setProducts(data))
-    }, [])
 
     useEffect(() => {
         const storedCart = getStoredCart();
