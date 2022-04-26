39d3b4c9a592f1e1686d73cb6ec92aab2f487f05
shoronshaha <smaranshaha@gmail.com>
2022-04-26 21:13:17 +0600

    connected local host server and work perfectly

--- src/components/orders/Orders.js
+++ src/components/orders/Orders.js
@@ -13,16 +13,16 @@ const Orders = () => {
     const navigate = useNavigate();
 
     const handleRemoveProduct = product => {
-        const rest = cart.filter(pd => pd.id !== product.id);
+        const rest = cart.filter(pd => pd._id !== product._id);
         setCart(rest);
-        removeFromDb(product.id);
+        removeFromDb(product._id);
     }
     return (
         <div className='shop-container'>
             <div className='review-items-container'>
                 {
                     cart.map(product => <ReviewItem
-                        key={product.id}
+                        key={product._id}
                         product={product}
                         handleRemoveProduct={handleRemoveProduct}
                     ></ReviewItem>)
